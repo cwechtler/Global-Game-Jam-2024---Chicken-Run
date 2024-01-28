cede5aa28849b2a7b5a5e07a6331974decb07a4a
Craig Wechtler <30483550+cwechtler@users.noreply.github.com>
2024-01-28 10:28:07 -0600

    add Speech Bubble follow

--- Assets/Scripts/NPC/Enemies/SpeechBubleFollow.cs
+++ Assets/Scripts/NPC/Enemies/SpeechBubleFollow.cs
@@ -1,17 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SpeechBubleFollow : MonoBehaviour
 {
     [SerializeField] private GameObject speechBubblePositionGO;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
         this.transform.position = speechBubblePositionGO.transform.position;
